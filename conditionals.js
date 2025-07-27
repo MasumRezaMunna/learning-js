ad1365361830558cd66560d4619861c61bc62799
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-27 21:12:39 +0600

    if else learned

---
+++ conditionals.js
@@ -0,0 +1,23 @@
+// if(3 < 10){
+//     console.log('i have a smaller value');
+// }
+// if(13 < 10){
+//     console.log('i have a smaller value');
+// }
+
+// var briyaniPrice = 500;
+// if(briyaniPrice < 300){
+//     console.log('mama give me a plate of biriyani');
+// }
+// var briyaniPrice = 250;
+// if(briyaniPrice < 300){
+//     console.log('mama give me a plate of biriyani');
+// }
+
+var weight = 20;
+if(weight <= 20){
+    console.log('I will carry it by myself')
+}
+else{
+    console.log('i will rent a rickshaw');
+}
