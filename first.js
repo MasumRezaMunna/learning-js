f75f5de5ab5754266fc789a58f3e417b3f3840ce
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-27 12:45:04 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,29 @@
+// those are number type variavle
+// console.log(3)
+// var weight = 38;
+// var price = 38;
+// var year = 1995;
+// var money = 2005;
+// var age;
+// console.log(age);
+// console.log(money);
+// var yo = 45;
+// yo =3;
+// console.log(yo);
+
+
+
+// those are string type variable
+// var name = "sadar uddin";
+// console.log(name);
+// var address = "sabuj nagar, joypurhat"
+// console.log(address);
+
+
+
+// those are boolean type  variable
+// var isRich = true;
+// console.log(isRich);
+// var passed = false;
+// console.log(passed);
+// console.log(typeof passed);
---
+++ index.js
@@ -0,0 +1,33 @@
+// var orangePrice = 20;
+// var chocolate = 0.5;
+// var applePrice = '300';
+// var applePrice = parseInt('300');
+// var applePrice = parseInt('300.55');
+// var applePrice = parseFloat('300.55');
+// console.log(typeof applePrice);
+// console.log(typeof chocolate);
+// console.log(orangePrice);
+// console.log(orangePrice + applePrice);
+// console.log(orangePrice + chocolate);
+
+
+
+
+
+// var first = 0.1;
+// var second = 0.2;
+// var total = first + second;
+// // console.log(total);
+// console.log(total.toFixed(2));
+
+// const num = 50/0;
+// console.log(num);
+
+
+var first = 'mobarok';
+var second = 'tobarok';
+var fullName = first + second;
+// console.log(fullName);
+
+console.log(40 + '20');
+console.log('50' - 10);
