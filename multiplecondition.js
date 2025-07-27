bbe482db387dd7d091ff85db96b12fc452c06625
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-27 21:38:12 +0600

    learned basic if else condition

---
+++ multiplecondition.js
@@ -0,0 +1,29 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+const hasCar = true;
+
+// if (salary > 20000 && height > 66 ) {
+//   console.log("su......patro");
+// } else {
+//   console.log("onno patro khuji");
+// }
+
+// if (salary > 25000 || height > 72) {
+//   console.log("eso baba kobul");
+// } else {
+//   console.log("vag tui mokbul");
+// }
+
+// if (salary > 25000 || height > 72 || isBCS == true) {
+//   console.log("eso baba kobul");
+// } else {
+//   console.log("vag tui mokbul");
+// }
+
+if (salary > 25000 && hasCar == true || isBCS != true) {
+    console.log('tomarr gusti raji');
+}
+else {
+    console.log('tumi bhago');
+}
