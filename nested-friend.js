1b54b78347a7dfdfe138838a13581086a4cde21c
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-29 18:23:24 +0600

    nested friend hw added

---
+++ nested-friend.js
@@ -0,0 +1,13 @@
+let score = 80;
+if(score > 80){
+    console.log("go for a lunch");
+}
+else if( score < 80 || score >= 60){
+    console.log("good luck next time");
+}
+else if( score < 60 || score >= 40){
+    console.log("message unseen");
+}
+else{
+    console.log("blocked");
+}
