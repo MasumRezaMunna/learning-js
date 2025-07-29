0b7fe15b71a02ef3852e7c53e4e4e1d2598a4adc
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-29 18:45:23 +0600

    updated nester friend

--- nested-friend.js
+++ nested-friend.js
@@ -1,12 +1,15 @@
-let score = 80;
+let score = 79;
+let inside = true;
+if(inside){
 if(score > 80){
     console.log("go for a lunch");
 }
-else if( score < 80 || score >= 60){
+else if( score < 80 && score >= 60){
     console.log("good luck next time");
 }
-else if( score < 60 || score >= 40){
+else if( score < 60 && score >= 40){
     console.log("message unseen");
+}
 }
 else{
     console.log("blocked");
