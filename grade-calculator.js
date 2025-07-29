34437dc79e229aa4eca395f76dd03b766ba358d7
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-29 18:15:00 +0600

    made a grade calculator

---
+++ grade-calculator.js
@@ -0,0 +1,16 @@
+let totalMarks = 81;
+if( totalMarks >= 90 && totalMarks <= 100){
+    console.log("A");
+}
+else if(totalMarks >= 80 && totalMarks <= 89){
+    console.log("B");
+}
+else if(totalMarks >= 70 && totalMarks <= 79){
+    console.log("C");
+}
+else if(totalMarks >= 60 && totalMarks <= 69){
+    console.log("D");
+}
+else{
+    console.log("F");
+}
