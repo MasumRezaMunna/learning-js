df3f31a2167d5ccfe4e95732512812ea5b41565f
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-31 00:22:15 +0600

    learned indexof

---
+++ Array/indexof.js
@@ -0,0 +1,13 @@
+const friends = ["balam", "kalam", "salam", "malam"]
+
+// console.log(friends.indexOf("salam"));
+// console.log(friends.indexOf("gelam"));
+
+const nums = [];
+const food = "rosogoolllaah";
+const age = 15;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
