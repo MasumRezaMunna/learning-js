cd5e77b2c9c9ec8f2e8d0f76e810c193c500eeb1
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-30 09:33:03 +0600

    learning loops

---
+++ loop.js
@@ -0,0 +1,12 @@
+// const fruits = ["apple", "komla", "lichu", "kola"];
+
+// for (const fruit of fruits){
+//     console.log(fruit);
+//     console.log("i want to eat");
+// }
+
+const numbers = [25, 55, 10, 68, 85];
+
+for (const num of numbers){
+    console.log(num);
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+const number = 7;
+if (number < 1){
+    console.log('choto ekta number paichi');
+}
+console.log('something new is cooking');
+
+let num = 0;
+while(num < 5){
+    console.log("looooping", num);
+    num = num + 1;
+}
