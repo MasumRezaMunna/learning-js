ee076a54f0a81918ba035486f3e7eeb866e6ac43
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-30 23:30:25 +0600

    learned includes method

---
+++ Array/includes.js
@@ -0,0 +1,11 @@
+const friends = ["balam", "kalam", "salam", "malam"]
+
+console.log(friends.includes("gelam"));
+console.log(friends.includes("balam"));
+
+if(friends.includes("salam")){
+    console.log("party");
+}
+else{
+    console.log("no food. we are fasting")
+}
