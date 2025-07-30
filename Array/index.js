ae03785b5124b6a3a6cf4e0595c1494b7618d0a3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-07-30 23:25:42 +0600

    learned push pop shift unshift

---
+++ Array/index.js
@@ -0,0 +1,10 @@
+const numbers = [1, 5, 66, 11, 45, 65, 65, 88, 11, 45, 65, 88, 11, 45, 65, 65, 88, 15];
+// console.log(numbers);
+
+// console.log(numbers[3])
+
+// const fourth = numbers[3];
+// console.log(fourth);
+
+numbers[1] = 999;
+console.log(numbers)
---
+++ Array/push-pop.js
@@ -0,0 +1,23 @@
+// const ages = [];
+// const numbers = [12, 87, 98, 45];
+
+// console.log(numbers);
+// numbers.push(44);
+// numbers.push(65, 11, 9);
+// console.log(numbers);
+
+// const friends = ["balam", "kalam", "salam", "malam"]
+// const out1 = friends.pop();
+// console.log(friends);
+// const out2 = friends.pop();
+// console.log(friends);
+// console.log(out1, out2);
+
+
+
+const friends = ["balam", "kalam", "salam", "malam"]
+console.log(friends);
+friends.shift();
+console.log(friends);
+friends.unshift("ghumailam")
+console.log(friends)
