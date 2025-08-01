6d09dd0d96adfb882520273b3c0bbe27935b4188
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-01 21:01:04 +0600

    add or remove elements

--- Array/hw.js
+++ Array/hw.js
@@ -1,4 +1,14 @@
-let friuts = ['apple', 'komla', 'licchu', 'kathal', 'kola'];
-console.log(friuts[2]);
-friuts[1] = 'jambura';
-console.log(friuts);
+// let friuts = ['apple', 'komla', 'licchu', 'kathal', 'kola'];
+// console.log(friuts[2]);
+// friuts[1] = 'jambura';
+// console.log(friuts);
+
+
+
+// 2.
+
+let touristDestination = ['paharpur', 'mohasthangor', 'joupurhat'];
+touristDestination.push('cox bazar'); 
+touristDestination.push('japhlong','sundarban'),
+touristDestination.pop('sundarban')
+console.log(touristDestination);
