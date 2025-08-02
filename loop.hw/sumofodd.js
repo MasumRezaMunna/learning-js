0454b4530db1cc46650fd721b15c9efed2df4a3f
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-02 16:18:34 +0600

    sum of odd and even numbers

---
+++ loop.hw/sumofodd.js
@@ -0,0 +1,18 @@
+// let sum = 0;
+// for( i = 91; i <= 129; i = i + 2){
+//     sum = sum + i;
+//     console.log(i);
+// }
+// console.log(("the sum is :"), sum);
+
+
+let sum = 0;
+
+for( i = 51; i <= 85; i++){
+    if(i % 2 === 0){ 
+        sum += i;
+        console.log(i);
+    }
+    
+}
+console.log("the sum is :", sum);
