ac3c368a14bcf6a9b100e10c65eca35df2d9c622
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-03 10:59:42 +0600

    multiplication-table using while

--- loop.hw/multiplication-table.js
+++ loop.hw/multiplication-table.js
@@ -1,6 +1,13 @@
-let n = 9;
-for (let i = 1; i <= 10; i++){
-    console.log(`${n} x ${i} = ${n * i}`)
+// let n = 9;
+// for (let i = 1; i <= 10; i++){
+//     console.log(`${n} x ${i} = ${n * i}`)
+// }
+
+
+let n = 5;
+i = 1;
+
+while(i <= 10){
+    console.log(`${n} x ${i} = ${n * i}`);
+    i++;
 }
-
-
