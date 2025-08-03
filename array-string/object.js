49281c50d1ca8465d8b306670d5bac63d9e4ec31
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-03 18:31:01 +0600

    objects

---
+++ array-string/object.js
@@ -0,0 +1,23 @@
+const age = 21;
+const school = 'high school';
+const isPassed = true;
+// let isDeveloper;
+
+const bottle = {
+    brand : 'apple',
+    price : 45,
+    color : 'white',
+    isClean : false
+}
+
+
+const subjects = {
+    name = 'biology',
+    teacher = 'rasheda mam',
+    examDate = '30 feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'finale exam',
+        marks: 100
+    }
+}
