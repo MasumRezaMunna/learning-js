47433e58a64349bf1ba3a1fd581b19bc911fe81d
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-03 10:21:07 +0600

    using while

--- loop.hw/commitment.js
+++ loop.hw/commitment.js
@@ -1,3 +1,11 @@
-for ( let day = 1; day <= 60; day++) {
+// for ( let day = 1; day <= 60; day++) {
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+// }
+
+
+
+let day = 1;
+while( day <=60){
     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    day++;
 }
