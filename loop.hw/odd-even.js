413d06e3a93547a66ec76a8c4329ae881d74abc2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-03 10:25:14 +0600

    using while and solved odd even

--- loop.hw/odd-even.js
+++ loop.hw/odd-even.js
@@ -1,7 +1,20 @@
-for( let i = 61; i <= 100; i = i + 2){
-    console.log(i);
-}
+// for( let i = 61; i <= 100; i = i + 2){
+//     console.log(i);
+// }
 
-for( let i = 78; i <= 98; i = i + 2){
+// for( let i = 78; i <= 98; i = i + 2){
+//     console.log(i);
+// }
+
+
+// let i = 61;
+// while(i <= 100){
+//     console.log(i);
+//     i += 2;
+// }
+
+let i = 78;
+while(i <= 98){
     console.log(i);
+    i += 2;
 }
