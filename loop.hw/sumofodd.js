09c679db5591028ba62a452018810a416a0c9794
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-03 10:55:11 +0600

    sum of odd numbers whit while loop

--- loop.hw/sumofodd.js
+++ loop.hw/sumofodd.js
@@ -6,13 +6,34 @@
 // console.log(("the sum is :"), sum);
 
 
-let sum = 0;
+// let sum = 0;
 
-for( i = 51; i <= 85; i++){
-    if(i % 2 === 0){ 
-        sum += i;
-        console.log(i);
-    }
+// for( i = 51; i <= 85; i++){
+//     if(i % 2 === 0){ 
+//         sum += i;
+//         console.log(i);
+//     }
     
+// }
+// console.log("the sum is :", sum);
+
+
+// let sum =0;
+// let i = 81;
+// while(i <= 131){
+//     sum += i;
+//     console.log(i);
+//     i += 2;
+// }
+// console.log("the sum is :", sum);
+
+
+
+let sum =0;
+let i = 206;
+while(i <= 311){
+    sum += i;
+    console.log(i);
+    i += 2;
 }
 console.log("the sum is :", sum);
