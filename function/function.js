6f675bfba79b45ffa6daab4f22b018f848e5a7bd
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-04 22:37:29 +0600

    learning how to use function

---
+++ function/function.js
@@ -0,0 +1,23 @@
+// function hello (){
+//     console.log("I am a Function");
+// }
+// hello();
+
+// function moneybag(){
+//     var money = 4500;
+//     console.log("I have " + money  + "Taka");
+// }
+// moneybag();
+
+function jogKorarMachine (input1, input2){
+
+    let sum = input1 + input2;
+    console.log("Sum is = " + sum);
+    return sum;
+}
+
+const result = jogKorarMachine(10, 40);
+console.log(result);
+
+// jogKorarMachine(10, 40);
+// jogKorarMachine(45, 4);
