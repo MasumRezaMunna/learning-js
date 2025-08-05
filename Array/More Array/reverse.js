105fa08a6cb52746832389a3bb6e6032825d87c6
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-05 23:49:14 +0600

    reverse in many ways

---
+++ Array/More Array/reverse.js
@@ -0,0 +1,23 @@
+const numbers = [1, 2, 3, 4, 5];
+const reversed = [];
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+//     reversed.unshift(numbers[i]);
+// }
+// console.log(reversed);
+
+// for (let i = numbers.length-1; i >= 0; i--){
+//     reversed.push(numbers[i]);
+// }
+// console.log(reversed);
+
+// for (const num of numbers){
+//     reversed.unshift(num);
+//     // console.log(num)
+// }
+// console.log(reversed);
+
+console.log(numbers);
+const result = numbers.reverse();
+console.log(numbers);
+console.log(result)
