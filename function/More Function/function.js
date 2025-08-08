ed5b43e80a55ebebda3c2e7aa42d1ddfb5fb85c2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-08 10:00:18 +0600

    function syntex

---
+++ function/More Function/function.js
@@ -0,0 +1,17 @@
+const number = 45;
+ 
+function fanOffKor(){
+    console.log('bosas theeke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the Fan');
+}
+
+fanOffKor();
+
+function brushTeeth () {
+    console.log('Pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso and fena felo');
+}
+
+brushTeeth();
