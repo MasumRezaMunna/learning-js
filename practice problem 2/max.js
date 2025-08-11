a3a981aa8095cbc4dace9841b6b11d69c2de194a
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-11 10:04:53 +0600

    max

---
+++ practice problem 2/max.js
@@ -0,0 +1,62 @@
+// const disha = 56;
+// const salman = 95;
+
+// if(disha > salman){
+//     console.log('Disha will get the strawberry');
+// }else{
+//     console.log('salman will eat the strawberry');
+// }
+
+// // inside a function
+// function getMax(num1, num2){
+//     if (num1 > num2){
+//         return num1;
+//     }else{
+//         return num2;
+//     }
+// }
+
+// const max1 = getMax (96, 79);
+// const max2 = getMax (56, 98);
+// const ultimateMax = getMax(max1, max2);
+
+// console.log('max of two is:', ultimateMax);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const jim = 99;
+const tim = 89;
+const kim = 68;
+
+if(jim > tim && jim > kim){
+    console.log('jim is the ultimate boss');
+}else if (tim > jim && tim > kim){
+    console.log('tim is the boss');
+}else{
+    console.log('kim is the kardashians boss')
+}
+
+
+
+
+
+function maxOfThree (num1, num2, num3){
+    // do it yourself
+}
+
+
+const max = Math.max(12, 1, 56, 5, 65, 8, 1, 99, 2);
+console.log('max isusing Math.max', max)
