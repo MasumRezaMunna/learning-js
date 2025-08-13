bb0b37c5374e0cc4c03ca7db80c91d1fbb40d241
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-13 13:55:55 +0600

    task 2 completed

--- Array/More Array/practice.js
+++ Array/More Array/practice.js
@@ -19,3 +19,19 @@
 // console.log(reverse);
 
 
+
+
+// Task-02
+
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// let evenNumbers = [];
+
+// for(let i = 0; i <= numbers.length; i++){
+//     if(numbers[i] % 2 === 0){
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+
+// console.log(evenNumbers);
+
+
