495365e7f78ad209ff7d49a6441762f1a5dfa6e9
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-15 21:43:46 +0600

    matched

---
+++ Array/More Array/matched.js
@@ -0,0 +1,40 @@
+const numbers = [45, 65, 23, 98, 19];
+
+// for(let i =0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     console.log(number);
+// }
+
+// for(const number of numbers){
+//     console.log(number);
+// }
+
+const products = [
+    {id: 1, name: 'Walton phone', price: 19000},
+    {id: 2, name: 'xiaomi phone', price: 19000},
+    {id: 3, name: 'mac book', price: 19000},
+    {id: 4, name: 'lenovo phone', price: 19000},
+    {id: 5, name: 'dell Laptop', price: 19000},
+    {id: 6, name: 'samsung phone', price: 19000},
+    {id: 7, name: 'nokia phone', price: 19000},
+    {id: 8, name: 'Realme Phone', price: 19000},
+];
+
+// for(const product of products){
+//     console.log(product)
+// }
+
+
+function matchedProducts (products, search){
+    const matched = [];
+    for(const product of products){
+        if (product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product);
+        }
+    }
+
+    return matched;
+}
+
+const result = matchedProducts(products, 'Laptop')
+console.log(result)
