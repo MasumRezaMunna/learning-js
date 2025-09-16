bdaac5f7d61b11d0f2cf2ea73e308ac809b013a9
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-16 08:01:07 +0600

    local storeage interactions for a shopping cart

--- local storage/cart.js
+++ local storage/cart.js
@@ -2,25 +2,43 @@ const handleAddProducts = () => {
   const productEl = document.getElementById("product");
   const quantityEl = document.getElementById("quantity");
   const product = productEl.value;
-  const quantity = quantityEl.value;
+  const quantity = parseInt(quantityEl.value);
+
+  
   console.log("product add", product, quantity);
 
   displayProducts(product, quantity);
+
+  addProductToCart(product, quantity)
 
   productEl.value = "";
   quantityEl.value = "";
 };
 
-const getCart = () =>{
-    const cart = {};
-    return cart;
-}
+const getCart = () => {
+  let cart = {};
+
+  const cartJSON = localStorage.getItem("cart");
+  if (cartJSON) {
+    cart = JSON.parse(cartJSON);
+  }
+  console.log(cartJSON)
+  return cart;
+};
 
 const addProductToCart = (product, quantity) => {
-    const cart = getCart();
+  const cart = getCart();
+  
+  if(cart[product]){
+    cart[product] = cart[product] + quantity
+  }else{
     cart[product] = quantity;
-    console.log('cart', cart)
-}
+  }
+
+  console.log("cart", cart);
+  const cartJSON = JSON.stringify(cart);
+  localStorage.setItem("cart", cartJSON);
+};
 
 const displayProducts = (product, quantity) => {
   const li = document.createElement("li");
@@ -30,3 +48,28 @@ const displayProducts = (product, quantity) => {
   const ul = document.getElementById("products-container");
   ul.appendChild(li);
 };
+
+
+// display products from stored local storage
+const displayStoredProducts = () => {
+  const cart = getCart();
+  for(const product in cart){
+    const quantity = cart[product]
+    console.log(product, quantity)
+    displayProducts(product, quantity)
+  }
+}
+
+displayStoredProducts();
+
+/**
+ * to save object / array in the local storage
+ * 1. convert the object to json string by using json.stringify
+ * 2. localstorage.setItem()
+ */
+
+/**
+ * to get object/array from the local storage
+ * 1. get the item from the local storage and it will be in JSON string
+ * 2. convert the JSON string to js object by using JSON.parse
+ */
